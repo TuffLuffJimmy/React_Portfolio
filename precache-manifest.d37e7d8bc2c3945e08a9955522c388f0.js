17b875bd560a611533cc5b78098ebdeffcbcc03d
Jimmy Echternach <jimmynechternach@gmail.com>
2020-06-26 11:43:58 -0700

    Updates

---
+++ precache-manifest.d37e7d8bc2c3945e08a9955522c388f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "645fa129e0b9f8943c068bf41bca4a5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a262dabb35468a320a7e",
+    "url": "/static/js/2.e69b1cce.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.e69b1cce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8b615f015367fe2b22c",
+    "url": "/static/js/main.541b8d8a.chunk.js"
+  },
+  {
+    "revision": "dd8147bbe7734801bdde",
+    "url": "/static/js/runtime-main.e312f208.js"
+  }
+]);
