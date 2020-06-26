e8f525762487a761ff7c824c0b48d0161890b47e
Jimmy Echternach <jimmynechternach@gmail.com>
2020-06-25 19:27:16 -0700

    added card and imported react bootstrap

---
+++ src/pages/Portfolio/Portfolio.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MyCard from '../../components/MyCard'
+
+const Portfolio = () => {
+  return (
+    <>
+    <h1>Portfolio</h1>
+      <MyCard title='CoZone' description='CoZone Description' link='https://tuffluffjimmy.github.io/CoZone/'/>
+    <MyCard />
+    </>
+  )
+}
+
+export default Portfolio
